4f7c52bfaffedde426a623ea92bf3d0f8597654e
Lv <506615869@qq.com>
2022-04-13 20:19:43 -0700

    test7

--- test.c
+++ test.c
@@ -2,7 +2,47 @@
 
 int main()
 {
-    printf("this is test6\n");
+    int arr[32] ;
+    int carry_bit = 1, flag = 0;
+    int n = 3, num = 0;
+    int i = 0, j = 0;
+    int carry_bit_tem = 0;
+    int a = 0, b = 4;
+
+    for (i = 0; i < 32; i++){
+        arr[i] = 0xffffffff;
+    }
+    
+    while(--b){
+        n = 3;
+        num = 0;
+        for( i = 0; i < 32; i++){
+            for(j = 0; j < 32 && num < 1000; j++){
+                num ++;
+
+                if (n == 3){
+                    a ++;
+                    n = 1;
+                    carry_bit_tem = carry_bit << j;
+                    arr[i] = arr[i] ^ carry_bit_tem;
+                    //printf("%d: %x, %x\n", j, arr[i], carry_bit_tem);
+                }else if (((carry_bit << j) & arr[i])){
+                    n++;
+                }
+                
+                if(a == 999){
+                    break;
+                }
+            }
+            
+            if(a == 999){
+                    break;
+                }
+            printf("%d: %x\n", i, arr[i]);
+        }
+    }
+
+    printf("%d, %d\n",num, a);
 
     return 0;
 }
